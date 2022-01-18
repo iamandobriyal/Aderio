cbca3e791360daf223ff39ab59e89df2f2508a0f
Aman Dobriyal <iamandobriyal@gmail.com>
2022-01-18 19:24:14 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+let serviceCard = document.querySelectorAll('.servicecard');
+let icon = document.querySelectorAll('i');
+console.log(serviceCard);
+for(let i=0; i<serviceCard.length; i++)
+{
+    serviceCard[i].addEventListener('mouseover',function()
+    {
+        icon[i].style.color = "white";
+    })
+    serviceCard[i].addEventListener('mouseout',function(){
+        icon[i].style.color = "black";
+    })
+}
+let slide = document.querySelectorAll('.slide');
+let dot = document.querySelectorAll('.dot');
+
+
+dot[0].style.background = "var(--links)";
+for(let i=0; i<slide.length; i++)
+{
+    dot[i].addEventListener('click',function(){
+        if(i==0)
+        {
+            slide[i].style.display = "flex";
+            slide[i+1].style.display = "none";
+            dot[i].style.background = "var(--links)";
+            dot[i+1].style.background = "white";
+        }
+        else{
+            slide[i].style.display = "flex";
+            slide[i-1].style.display = "none";
+            dot[i].style.background = "var(--links)";
+            dot[i-1].style.background = "white";
+        }
+    })
+}
+
+let faqCard = document.querySelectorAll('.faq');
+let des = document.querySelectorAll('des');
+let faqopen = document.querySelectorAll('.add');
+let faqoff = document.querySelectorAll('.sub');
+for(let i=0; i<faqCard.length; i++)
+{
+    faqopen[i].addEventListener('click',function(){
+        des[i].style.display = "block";
+        faqopen[i].style.display = "none";
+        faqoff[i].style.display = "block";
+    })
+    faqoff[i].addEventListener('click',function(){
+        des[i].style.display = "none";
+        faqoff[i].style.display = "none";
+        faqopen[i].style.display = "block";
+    })
+}
