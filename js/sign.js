c3a3c44a6a247e43b7b591e4fa93a3940e36c3d7
Aman Dobriyal <iamandobriyal@gmail.com>
2022-01-19 10:22:14 +0530

    kjk

--- js/sign.js
+++ js/sign.js
@@ -1,16 +1,3 @@
-let forbtn = document.querySelector('#forbtn');
-let card = document.querySelectorAll('.card');
-forbtn.addEventListener('click',function()
-{
-    card[0].style.display = "none";
-    card[1].style.display = "none";
-    card[2].style.display = "block";
+document.querySelector('#switch').addEventListener('click',function(){
+    location.href = "../index.html";
 })
- let forget = document.querySelector('.forget');
-
- forget.addEventListener('click',function()
- {
-    card[0].style.display = "block";
-    card[1].style.display = "block";
-    card[2].style.display = "none";
- })
