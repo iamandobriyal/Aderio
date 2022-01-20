074bc7e419c1001c655f6b800baff02e9a4f5016
Aman Dobriyal <iamandobriyal@gmail.com>
2022-01-20 19:18:05 +0530

    Update sign.js

--- js/sign.js
+++ js/sign.js
@@ -5,3 +5,14 @@ let logo = document.querySelector('.logo');
 logo.addEventListener('click',function(){
     location.href = "../index.html";
 })
+
+let menu = document.querySelectorAll('menu-item');
+
+let links = ["../index.html","../pages/about.html","../index.html#services","../index.html#contact","../pages/dashboard.html"];
+
+for(let i=0; i<menu.length; i++)
+{
+    menu[i].addEventListener('click',function(){
+        location.href = links[i];
+    })
+}
